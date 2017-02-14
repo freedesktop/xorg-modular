92cdfec28bea53c4be2c9c1de1e02b740cee34b3
Emil Velikov <emil.velikov@collabora.com>
2017-02-14 11:42:57 +0000

    x-driver-screen-scrn-conv.sh: add shebang
    Signed-off-by: Emil Velikov <emil.velikov@collabora.com>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- x-driver-screen-scrn-conv.sh
+++ x-driver-screen-scrn-conv.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 find . -name \*.[ch] -exec sed -i -e's/xf86Screens\[\([a-z].*\)->myNum\]/xf86ScreenToScrn(\1)/g' {} \;
 find . -name \*.[ch] -exec sed -i -e's/screenInfo.screens\[\([a-z].*\)->scrnIndex\]/xf86ScrnToScreen(\1)/g' {} \;
